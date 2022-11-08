7a11c54eb15166547b0f66b2fc846a0aa96373e1
younghwan12 <dowk3432@gmail.com>
2022-11-08 12:11:42 +0900

    2022.11.08

---
+++ src/components/TitleCont.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="cont__title">
+    <div class="container">
+      <h1>
+        <strong>{props.title[0]}</strong>
+        <em>{props.title[1]}</em>
+      </h1>
+    </div>
+  </section>
+</template>
---
+++ src/views/MainView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <HeaderCont />
+    <section class="cont__main">
+      <div class="container">
+        <div class="main__inner">
+          <div v-for="title in titles" v-bind:key="title.text">
+            {{ title.text }}
+          </div>
+        </div>
+      </div>
+    </section>
+    <FooterCont />
+  </div>
+</template>
+
+<script>
+import HeaderCont from "@/components/HeaderCont.vue";
+import FooterCont from "@/components/FooterCont.vue";
+
+export default {
+  components: {
+    HeaderCont,
+    FooterCont,
+  },
+
+  data: function () {
+    return {
+      titles: [
+        { text: "we provide" },
+        { text: "visual coding" },
+        { text: "solutions" },
+        { text: "for you API" },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.main__inner {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  height: 100vh;
+  color: var(--black);
+
+  > div {
+    font-size: 8vw;
+    line-height: 1;
+    text-transform: uppercase;
+  }
+}
+</style>
